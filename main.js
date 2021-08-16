e0d91e4840aab35e72115ea7a2a9672db6371fd9
Kemuel12 <73594961+Kemuel12@users.noreply.github.com>
2021-08-16 07:52:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+LeftWristX=0;
+RightWristX=0;
+Difference=0;
+
+function setup()
+{
+     canvas=createCanvas(550,500)
+     canvas.position(560,100)
+
+     video=createCapture(550,550)
+     video.size(560,500)
+
+     poseNet=ml5.poseNet('pose', modelLoaded)
+     poseNet.on('pose', gotPoses)
+}
+
+function draw()
+{
+    background('red');
+    document.getElementById("text_side_width").innerHtml="Font size of the text will be = "+Difference+"px";
+    text("WhiteHatJR",300,200);
+}
+
+function modelLoaded()
+{
+    console.log("Posenet is initialised")
+}
+
+function gotPoses(results)
+{
+ if(results.length>0)
+ {
+     console.log(results);
+     LeftWristX=results[0].pose.leftWrist.x;
+     RightWristX=results[0].pose.rightWrist.x;
+     Difference=floor(LeftWristX-RightWristX);
+     console.log("LeftWristX="+LeftWristX+"RightWristX="+RightWristX+"Difference="+Difference);
+     
+ }
+}
